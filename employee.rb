78b88e74c84d0461c25078393170a101b25fedce
Joseph DePompeis <josephdepompeis@gmail.com>
2016-02-15 02:59:36 -0500

    commited really rough code for parsing txt document, some regex implemented as well, the txt parser not connected to latest review-reviewer

--- employee.rb
+++ employee.rb
@@ -40,31 +40,90 @@ class Employee
     @review_file << contents
   end
 
+  def add_review_file_updated(review_text)
+    contents = File.open(review_text)
+    review1 = []
+    review2 = []
+    review3 = []
+    review4 = []
+    counter = 0
+    contents.each_line do |s|
+      if s.to_s.include?("name-") && counter == 0
+        review1 << s
+        counter += 1
+      elsif counter == 1
+        review1 << s
+        counter += 1
+      elsif counter == 2
+        review1 << s
+        counter += 1
+      elsif counter == 3
+        review1 << s
+        counter += 1
+      elsif counter == 4
+        review1 << s
+        counter += 1
+      elsif s.to_s.include?("name-") && counter == 5
+        review2 << s
+        counter +=1
+      elsif counter == 6
+        review2 << s
+        counter += 1
+      elsif counter == 7
+        review2 << s
+        counter += 1
+      elsif counter == 8
+        review2 << s
+        counter += 1
+      elsif counter == 9
+        review2 << s
+        counter += 1
+      elsif s.to_s.include?("name-") && counter == 10
+        review3 << s
+        counter += 1
+      elsif  counter == 11
+        review3 << s
+        counter += 1
+      elsif counter == 12
+        review3 << s
+        counter += 1
+      elsif counter == 13
+        review3 << s
+        counter += 1
+      elsif counter == 14
+        review3 << s
+        counter += 1
+      elsif s.to_s.include?("name-") && counter == 15
+        review4 << s
+        counter += 1
+      elsif counter == 16
+        review4 << s
+        counter +=1
+      elsif counter == 17
+        review4 << s
+        counter += 1
+      elsif counter == 18
+        review4 << s
+        counter += 1
+      else
+        puts "does not compute"
+      end
+    end
+      review1.each {|s| @review << review4.to_s if s.to_s.include?("#{@name}")}
+      review2.each {|s| @review << review4.to_s if s.to_s.include?("#{@name}")}
+      review3.each {|s| @review << review4.to_s if s.to_s.include?("#{@name}")}
+      review4.each {|s| @review << review4.to_s if s.to_s.include?("#{@name}")}
+  end
+
   def scan_review_file
-    # @review_file.scan(/mason/i)
-    #positive terms
     positive_terms = []
     negative_terms = []
-    @review_file.scan(Regexp.union(/good/, /satisfactory/, /positive/, /hard/)).each do |match|
+    @review_file.scan(Regexp.union(/good/, /\Wsatisfactory/, /positive/, /hard/, /hard.work/, /swell/)).each do |match|
       positive_terms << match
       end
     @review_file.scan(Regexp.union(/bad/, /unsatisfactory/, /late/, /slow/)).each do |match|
       negative_terms << match
       end
     @satisfactory = true if positive_terms.length > negative_terms.length
-
-    end
-
-
-
   end
-
-
-
-
-  #   @review << review_text
-  # end
-  # reviews = ['sample_review.txt']
-  # # reviews = ['sample_review.txt', 'your_file_here.txt']
-
-# end
+end
